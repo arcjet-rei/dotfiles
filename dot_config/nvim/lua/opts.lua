56bfa4c0d4f52b0ff36d7c0b066b4a6efcd6aa07
rei <forrest@arcjet.com>
2025-07-30 00:16:35 -0700

    Try spell checking.

--- dot_config/nvim/lua/opts.lua
+++ dot_config/nvim/lua/opts.lua
@@ -14,26 +14,29 @@ vim.opt.termguicolors = true
 -- noselect: Do not select, force to select one from the menu
 -- shortness: avoid showing extra messages when using completion
 -- updatetime: set updatetime for CursorHold
-vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
-vim.opt.shortmess = vim.opt.shortmess + {c = true}
-vim.api.nvim_set_option('updatetime', 300) 
+vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
 
 -- Fixed column for diagnostics to appear
 -- Show autodiagnostic popup on cursor hover_range
--- Goto previous / next diagnostic warning / error 
--- Show inlay_hints more frequently 
+-- Goto previous / next diagnostic warning / error
+-- Show inlay_hints more frequently
 vim.cmd([[
 set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focusable = false})
 ]])
 
-vim.opt.switchbuf = { 'usetab' }
+vim.opt.switchbuf = { "usetab" }
 
 -- search tweaks
-vim.opt.ignorecase = true   -- ignore case in searches by default
-vim.opt.smartcase = true    -- case sensitive if you include upper case
-vim.opt.infercase = true    -- fix completion to work the same as search + smartcase
+vim.opt.ignorecase = true -- ignore case in searches by default
+vim.opt.smartcase = true -- case sensitive if you include upper case
+vim.opt.infercase = true -- fix completion to work the same as search + smartcase
 
 -- UI tweaks
-vim.opt.wildmode = "full"   -- explicitly set wildmode
-vim.opt.wildmenu = true     -- use enhanced command completion
+vim.opt.wildmode = "full" -- explicitly set wildmode
+vim.opt.wildmenu = true -- use enhanced command completion
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
