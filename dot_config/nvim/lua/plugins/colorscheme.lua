dd87a0924aba17303aedd2b593246f84c35aa8fb
rei <forrest@arcjet.com>
2025-08-01 08:15:54 -0700

    chore(deps): update lockfiles, add copilot

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,16 @@ return {
   {
     "tssm/fairyfloss.vim",
   },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   -- this is how you tell LazyVim to use a different color scheme
   {
     "LazyVim/LazyVim",
