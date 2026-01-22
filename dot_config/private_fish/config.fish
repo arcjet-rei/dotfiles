927954eafefaf4c8ac023e6c3fd430fd4eb2679a
零Rei <rei@arcjet.com>
2026-01-22 08:13:48 -0800

    feat: add OCaml to play around with WebAssembly stuff

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -13,3 +13,12 @@ set PATH $PATH /Users/rei/.local/bin
 
 # run this last so that mise's paths take priority
 mise activate fish | source
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/rei/.opam/opam-init/init.fish' && source '/Users/rei/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
